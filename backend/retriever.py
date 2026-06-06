9c6e9daeeb9a0a58af748c976d5f6c24d65f9c09
Aadhithyau07 <aadhithyau07@gmail.com>
2026-06-06 19:11:20 +0530

    updated comments for easier understanding

--- backend/retriever.py
+++ backend/retriever.py
@@ -1,15 +1,16 @@
-import chromadb
+import chromadb #Vector database.
 
 client = chromadb.PersistentClient(
-    path="./my_chromadb"
+    path="./my_chromadb" 
 )
 
 collection = client.get_or_create_collection(
     name="research_papers"
-)
+)#Loading of database get_or_create_collection is used instead of get_collection as I got the error when I tried to deploy it in the browser
+#It would work for get_collection in our case.
 
 
-def retrieve_documents(query, n_results=20):
+def retrieve_documents(query, n_results=20):#retrieval of chunks from chroma db from the query with suitable semantic meaning
 
     result = collection.query(
         query_texts=[query],
